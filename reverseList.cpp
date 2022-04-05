d5d829d20b9ab296aad5d73a3f5cd69568260558
yyx <2361983316@qq.com>
2022-04-06 02:21:21 +0800

    链表原地反转

---
+++ reverseList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *pre = nullptr;
+        ListNode *cur = head;
+        while (cur)
+        {
+            ListNode *next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
+int main()
+{
+
+    Solution sol;
+    cout < < < < endl;
+    return 0;
+}
